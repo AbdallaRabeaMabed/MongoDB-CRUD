eeb849bc64b5a84b21a758e5e10ea7ce1b47d867
Abdalla Rabea Mabed <abdallamabed407@gmail.com>
2025-10-25 16:13:41 +0300

    adding route to get a specific book

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { connectToDb ,getDb} = require('./db');
+const { ObjectId } = require('mongodb');
 
 //initialize express app and middleware
 const app = express();
@@ -28,5 +29,23 @@ app.get('/books', (req, res) => {
     .catch(() => {
       res.status(500).json({ error: 'Could not fetch the documents' });
     });
+
+
+});
+
+// route to get a single book by id
+app.get('/books/:id', (req, res) => {
+  if(ObjectId.isValid(req.params.id)){
+    db.collection('books')
+    .findOne({_id: new ObjectId(req.params.id)})
+    .then(doc => {
+      res.status(200).json(doc);
+    })
+    .catch(() => {
+      res.status(500).json({ error: 'Could not fetch the document' });
+    });
+  }else{
+    res.status(500).json({ error: 'Not a valid document id' });
+  }
 
 });
