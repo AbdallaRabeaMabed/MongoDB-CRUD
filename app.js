d8253bd1b723ef03f01daba68fab0d34f4adbbe7
Abdalla Rabea Mabed <abdallamabed407@gmail.com>
2025-10-29 08:10:43 +0300

    adding route to update a book by id and tested via postman

--- app.js
+++ app.js
@@ -80,3 +80,21 @@ app.delete('/books/:id', (req, res) => {
   }
 
 });
+
+//route to update a book by id
+app.patch('/books/:id', (req, res) => {
+  const updates = req.body;
+  if(ObjectId.isValid(req.params.id)){
+    db.collection('books')
+      .updateOne({_id: new ObjectId(req.params.id)}, {$set: updates})
+      .then(result => {
+        res.status(200).json(result);
+      })
+      .catch(() => {
+        res.status(500).json({ error: 'Could not update the document' });
+      });
+  }else{
+    res.status(500).json({ error: 'Not a valid document id' });
+  }
+
+});
